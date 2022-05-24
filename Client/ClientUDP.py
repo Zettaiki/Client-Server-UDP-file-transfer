68db7925b5332b3a0e5dd39ffae873bee6e48209
Pablo Vargas <psebastian2001@hotmail.com>
2022-05-24 12:43:28 +0200

    Implement ack in get and put methods
    For debugging and faster testing, implement delete (from remote and local) and exit methods.

--- Client/ClientUDP.py
+++ Client/ClientUDP.py
@@ -1,5 +1,6 @@
 from socket import *
 import os
+import time
 
 BUFFER_SIZE = 1024
 
@@ -22,67 +23,106 @@ def getFromServer():
         return
     
     filename, server_address = client_socket.recvfrom(BUFFER_SIZE)
-    download_data = open("./Client/" + filename.decode('utf-8'), 'wb')
+    download_data = open(filename.decode('utf-8'), 'wb')
     packet, server_address = client_socket.recvfrom(BUFFER_SIZE)
+    packet_received = 0
     try:
         while(packet):
+            packet_received = packet_received + 1
             download_data.write(packet)
             client_socket.settimeout(2)
             packet, server_address = client_socket.recvfrom(BUFFER_SIZE)
     except timeout:
         download_data.close()
-        print(">> File Downloaded!")
+        print(">> Checking file integrity...")
+        client_socket.settimeout(5)
+        ack_num, server_address = client_socket.recvfrom(BUFFER_SIZE)
+        if packet_received == int(ack_num.decode('utf-8')):
+            print(">> File Downloaded!")
+            return
+        print(">> [Error]: Packet loss, file corrupted. Deleting file and ending process")
+        os.remove(filename)
         return
 
 # Method that send the file requested to the server.
 def sendToServer(filename):
-    print(">> Querying client filenames")
-    dirFiles = os.listdir('./Client')
-
-    if len(dirFiles) <= 1:
-        message = '1'
-        client_socket.sendto(message.encode('utf-8'), server_address)
-        return
-
-    for dirFileName in dirFiles:
-        if filename == dirFileName:
-            print(">> File found! ->", filename)
-            message = '0'
-            client_socket.sendto(message.encode('utf-8'), server_address)
-            data = open("./Client/" + filename, 'rb')
-            print(">> Sending filename...")
-            client_socket.sendto(filename.encode('utf-8'), server_address)
+    data = open(filename, 'rb')
+    print(">> Sending filename...")
+    client_socket.sendto(filename.encode('utf-8'), server_address)
+    packet = data.read(BUFFER_SIZE)
+    print(">> Sending data...")
+    packet_sent = 0
+    while (packet):
+        if(client_socket.sendto(packet, server_address)):
+            packet_sent = packet_sent + 1
+            print(">> Sending...")
             packet = data.read(BUFFER_SIZE)
-            print(">> Sending data...")
-            while (packet):
-                if(client_socket.sendto(packet, server_address)):
-                    print(">> Sending...")
-                    packet = data.read(BUFFER_SIZE)
-            data.close()
-            print(">> Sending successful!")
-            return
-    message = '2'
-    client_socket.sendto(message.encode('utf-8'), server_address)
+    data.close()
+    time.sleep(3)
+    client_socket.sendto(str(packet_sent).encode('utf-8'), server_address)
+    print(">> Sending complete!")
     return
+
+# Method that check if file exist in directory. If not, print the error and end the process.
+def checkFileExist(filename):
+    print(">> Querying filenames in directory...")
+
+    if len(os.listdir()) == 0:
+        print(">> [Error]: There are no files in the directory!")
+        return False
+
+    if os.path.exists(filename):
+        print('>> File found! ->', filename)
+        return True
+
+    print(">> [Error]: File not found!")
+    return False
 
 # Main loop to send the order to the server.
 while True:
     client_message = input("\n>> To server => ")
-    client_socket.sendto(client_message.encode('utf-8'), server_address)
-
     client_request = client_message.split()
 
     if client_request[0] == 'list':
+        client_socket.sendto(client_message.encode('utf-8'), server_address)
         print(">> Getting list from server")
         list, server_address = client_socket.recvfrom(BUFFER_SIZE)
         print(">> List of files in server: " , list.decode('utf-8'))
         continue
     
     if client_request[0] == 'get':
+        client_socket.sendto(client_message.encode('utf-8'), server_address)
         print(">> Querying file...")
         getFromServer()
         continue
     
     if client_request[0] == 'put':
-        print(">> Sending file to server...")
-        sendToServer(client_request[1])
+        if checkFileExist(client_request[1]):
+            print(">> Sending file to server...")
+            client_socket.sendto(client_message.encode('utf-8'), server_address)
+            sendToServer(client_request[1])
+
+    if client_request[0] == 'removelocal':
+        try:
+            os.remove(client_request[1])
+            print(">> ", client_request[1], " deleted. [local]")
+            continue
+        except FileNotFoundError:
+            print(">> [Error]: File not found. [local]")
+            continue
+
+    if client_request[0] == 'removeserver':
+        client_socket.sendto(client_message.encode('utf-8'), server_address)
+        status, server_address = client_socket.recvfrom(BUFFER_SIZE)
+        if int(status.decode('utf-8')) == 0:
+            print(">> ", client_request[1], " deleted. [remote]")
+        print(">> [Error] File not found. [remote]")
+
+        continue
+
+    if client_request[0] == 'exit':
+        exit()
+
+    if client_request[0] == 'exitserver':
+        client_socket.sendto(client_message.encode('utf-8'), server_address)
+        continue
--- Server/ServerUDP.py
+++ Server/ServerUDP.py
@@ -1,5 +1,6 @@
 from socket import *
 import os
+import time
 
 BUFFER_SIZE = 1024
 
@@ -14,81 +15,113 @@ print(">> Server online.")
 
 # Method that send the file requested to the client.
 def sendToClient(filename, client_address):
-    print(">> Querying server filenames")
-    dirFiles = os.listdir('./Server')
-
-    if len(dirFiles) <= 1:
-        message = '1'
-        server_socket.sendto(message.encode('utf-8'), client_address)
-        return
-    
-    for dirFileName in dirFiles:
-        if filename == dirFileName:
-            print(">> File found! ->", filename)
-            message = '0'
-            server_socket.sendto(message.encode('utf-8'), client_address)
-            data = open("./Server/" + filename, 'rb')
-            print(">> Sending filename...")
-            server_socket.sendto(filename.encode('utf-8'), client_address)
+    data = open(filename, 'rb')
+    print(">> Sending filename...")
+    server_socket.sendto(filename.encode('utf-8'), client_address)
+    packet = data.read(BUFFER_SIZE)
+    print(">> Sending data...")
+    packet_sent = 0
+    while (packet):
+        if(server_socket.sendto(packet, client_address)):
+            packet_sent = packet_sent + 1
+            print(">> Sending...")
             packet = data.read(BUFFER_SIZE)
-            print(">> Sending data...")
-            while (packet):
-                if(server_socket.sendto(packet, client_address)):
-                    print(">> Sending...")
-                    packet = data.read(BUFFER_SIZE)
-            data.close()
-            print(">> Sending successful!")
-            return
-    message = '2'
-    server_socket.sendto(message.encode('utf-8'), client_address)
+    data.close()
+    time.sleep(3)
+    server_socket.sendto(str(packet_sent).encode('utf-8'), client_address)
+    print(">> Sending complete!")
     return
 
 # Method that gets a file and store it in the server
 def getFromClient(filename):
-    status, client_address = server_socket.recvfrom(BUFFER_SIZE)
-    if int(status.decode('utf-8')) == 1:
-        print(">> Aborting request: the user directory is empty.")
-        return
-    if int(status.decode('utf-8')) == 2:
-        print(">> Aborting request: the user don't have files to upload.")
-        return
-    
     filename, client_address = server_socket.recvfrom(BUFFER_SIZE)
-    download_data = open("./Server/" + filename.decode('utf-8'), 'wb')
+    download_data = open(filename.decode('utf-8'), 'wb')
     packet, client_address = server_socket.recvfrom(BUFFER_SIZE)
+    packet_received = 0
     try:
         while(packet):
+            packet_received = packet_received + 1
             download_data.write(packet)
             server_socket.settimeout(2)
-            packet, server_address = server_socket.recvfrom(BUFFER_SIZE)
-    except timeout:
+            packet, client_address = server_socket.recvfrom(BUFFER_SIZE)
+    except TimeoutError:
         download_data.close()
-        print(">> File Downloaded!")
-
+        print(">> Checking file integrity...")
+        server_socket.settimeout(5)
+        ack_num, client_address = server_socket.recvfrom(BUFFER_SIZE)
+        if packet_received == int(ack_num.decode('utf-8')):
+            print(">> File Downloaded!")
+            return
+        
         # TODO: set timeout to default
-
+        print(">> [Error]: Packet loss, file corrupted. Deleting file and ending process")
+        os.remove(filename)
         return
+
+# Method that check if file exist in directory. If not, send the error code to client.
+def checkFileExist(filename, client_address):
+    print(">> Querying filenames in directory...")
+
+    if len(os.listdir('.')) == 0:
+        print(">> [Error]: Empty directory, error code = 1")
+        message = '1'
+        server_socket.sendto(message.encode('utf-8'), client_address)
+        return False
+    
+    if os.path.exists(filename):
+        print('>> File found! ->', filename)
+        message = '0'
+        server_socket.sendto(message.encode('utf-8'), client_address)
+        return True
+
+    print(">> [Error]: File not found, error code = 2")
+    message = '2'
+    server_socket.sendto(message.encode('utf-8'), client_address)
+    return False
 
 # Main loop to get the order from the client.
 while True:
     print("\n>> Waiting for client...")
-    client_message, client_address = server_socket.recvfrom(BUFFER_SIZE)
+    server_socket.settimeout(30)
+    client_message = ""
+    try:
+        client_message, client_address = server_socket.recvfrom(BUFFER_SIZE)
+    except TimeoutError:
+        continue
     print("\n>> Client message: ", client_message.decode('utf-8'))
 
     client_request = client_message.decode('utf-8').split()
     
     if client_request[0] == 'list':
         print(">> Getting list of files")
-        file_list = os.listdir('./Server')
+        file_list = os.listdir('.')
         print(">> Sending list to ", client_address)
         server_socket.sendto(str(file_list).encode('utf-8'), client_address)
         print(">> Completed!")
         continue
     
     if client_request[0] == 'get':
-        sendToClient(client_request[1], client_address)
-        continue
+        if checkFileExist(client_request[1], client_address):
+            sendToClient(client_request[1], client_address)
+            continue
 
     if client_request[0] == 'put':
         getFromClient(client_request[1])
         continue
+    
+    if client_request[0] == 'removeserver':
+        print('>> Deleting ', client_request[1])
+        try:
+            os.remove(client_request[1])
+            message = '0'
+            server_socket.sendto(message.encode('utf-8'), client_address)
+            print('>> ', client_request[1], " deleted.")
+            continue
+        except FileNotFoundError:
+            message = '1'
+            server_socket.sendto(message.encode('utf-8'), client_address)
+            print(">> [Error] File not found.")
+            continue
+    
+    if client_request[0] == 'exitserver':
+        exit()
